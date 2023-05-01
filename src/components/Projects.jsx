07ed2260a4a420f090345b295341188273f4bc3e
Lkanath Panda <lokanthpanda128@gmai.com>
2023-05-01 20:39:54 +0530

    project heading added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,10 @@ import './Projects.css'
 const Projects = () => {
   return (
     <>
-    
+  <div className="head">
+    <h1>Projects</h1>
+    <h3>This week</h3>
+  </div>
     <div className="box">
       <div className="box-heading">
       <span>To do</span>
@@ -18,6 +21,38 @@ const Projects = () => {
         <h2>Projects</h2>
     </div>
     </div>
+
+    <div className="box2">
+      <div className="box-heading">
+      <span>In progress</span>
+      </div>
+      <div className="bar">
+        <span>+</span>
+      </div>
+    <div className="container">
+        <h2>Projects</h2>
+    </div>
+    <div className="container1">
+        <h2>Projects</h2>
+    </div>
+    </div>
+
+    <div className="box3">
+      <div className="box-heading">
+      <span>Completed</span>
+      </div>
+      <div className="bar">
+        <span>+</span>
+      </div>
+    <div className="container">
+        <h2>Projects</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem eos nesciunt dolor quidem id in ipsam voluptatem modi ratione soluta ipsa, earum vero. Nesciunt rem vel quas necessitatibus deserunt cum.</p>
+    </div>
+    {/* <div className="container1">
+        <h2>Projects</h2>
+    </div> */}
+    </div>
+
     </>
   )
 }
